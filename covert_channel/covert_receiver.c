529bf7109de110475c70bd7122c97c27864e54ff
Hendrikto <hendrik.to@gmail.com>
2017-12-05 18:19:47 +0100

    Use inotify to implement an event based receiver.
    * The receiver blocks while waiting for attribute changes.

--- covert_channel/covert_receiver.c
+++ covert_channel/covert_receiver.c
@@ -1,16 +1,25 @@
 // author: Hendrik Werner s4549775
 
+#include <limits.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/inotify.h>
 #include <sys/stat.h>
 
+#define CHANNEL "./channel"
+#define EVENT_BUFFER_SIZE sizeof(struct inotify_event) + NAME_MAX + 1
+
 int main() {
+	char events[EVENT_BUFFER_SIZE];
 	struct stat *file_stat = malloc(sizeof(struct stat));
+	int inotify_fd = inotify_init();
+	inotify_add_watch(inotify_fd, CHANNEL, IN_ATTRIB);
 	for (;;) {
-		stat("./channel", file_stat);
+		read(inotify_fd, events, EVENT_BUFFER_SIZE);
+		stat(CHANNEL, file_stat);
 		printf("Received: %d\n", file_stat->st_mode & 0777);
-		sleep(1);
 	}
 	free(file_stat);
+	close(inotify_fd);
 }
